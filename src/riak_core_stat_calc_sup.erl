b117f5796131d31f3f39bf4b08826f553856c36b
Russell Brown <russell.brown@mac.com>
2012-11-05 10:20:52 +0000

    Add a per stat calculation process for fine grained control
    Per stat process for fine grained concurrency control of stat
    calculation and caching.
    
    A simple 'query' module for selecting stats by path from folsom
    A simple REST interface to fetch stats by path

--- src/riak_core_stat_calc_sup.erl
+++ src/riak_core_stat_calc_sup.erl
@@ -1,8 +1,5 @@
 %% -------------------------------------------------------------------
-%%
-%% riak_core: Core Riak Application
-%%
-%% Copyright (c) 2007-2010 Basho Technologies, Inc.  All Rights Reserved.
+%% Copyright (c) 2007-2011 Basho Technologies, Inc.  All Rights Reserved.
 %%
 %% This file is provided to you under the Apache License,
 %% Version 2.0 (the "License"); you may not use this file
@@ -19,37 +16,36 @@
 %% under the License.
 %%
 %% -------------------------------------------------------------------
-
--module(riak_core_stat_sup).
-
+-module(riak_core_stat_calc_sup).
 -behaviour(supervisor).
-
-%% API
--export([start_link/0]).
-
-%% Supervisor callbacks
--export([init/1]).
+-export([start_link/0, init/1]).
+-export([calc_proc/1, stop_proc/1]).
 
 %% Helper macro for declaring children of supervisor
--define(CHILD(I, Type, Timeout), {I, {I, start_link, []}, permanent, Timeout, Type, [I]}).
+-define(CHILD(I, Type, Timeout), {I, {riak_core_stat_calc_proc, start_link, [I]}, permanent, Timeout, Type, [riak_core_stat_calc_proc]}).
 -define(CHILD(I, Type), ?CHILD(I, Type, 5000)).
-
-%% ===================================================================
-%% API functions
-%% ===================================================================
 
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
-%% ===================================================================
-%% Supervisor callbacks
-%% ===================================================================
-
 init([]) ->
-    Children = lists:flatten(
-                 [?CHILD(folsom_sup, supervisor),
-                  ?CHILD(riak_core_stats_sup, supervisor),
-                  ?CHILD(riak_core_stat_calc_sup, supervisor)
-                 ]),
+    {ok, {{one_for_one, 5, 10}, []}}.
 
-    {ok, {{rest_for_one, 10, 10}, Children}}.
+%% start a process for the given stat
+%% @see `riak_core_stat_calc_proc`
+calc_proc(Stat) ->
+    Ref = calc_proc_ref(Stat),
+    Pid = case supervisor:start_child(?MODULE, Ref) of
+              {ok, Child} -> Child;
+              {error, {already_started, Child}} -> Child
+          end,
+    Pid.
+
+stop_proc(Stat) ->
+    supervisor:terminate_child(?MODULE, Stat),
+    supervisor:delete_child(?MODULE, Stat),
+    ok.
+
+%% @private
+calc_proc_ref(Stat) ->
+    ?CHILD(Stat, worker).
