f8e7f506c39188a80b41d2dde5c2f3f775393d39
Sean Cribbs <sean@basho.com>
2013-01-17 14:42:57 -0700

    Add vclock:dominates/2 with property.

--- test/vclock_qc.erl
+++ test/vclock_qc.erl
@@ -26,14 +26,17 @@ command(#state{vclocks=Vs}) ->
     oneof([{call, ?MODULE, fresh, []}] ++
           [{call, ?MODULE, increment, [gen_actor_id(), elements(Vs)]} || length(Vs) > 0] ++
           [{call, ?MODULE, get_counter, [gen_actor_id(), elements(Vs)]} || length(Vs) > 0] ++
-          [{call, ?MODULE, merge, [list(elements(Vs))]} || length(Vs) > 0] ++ 
-          [{call, ?MODULE, descends, [elements(Vs), elements(Vs)]} || length(Vs) > 0]
+          [{call, ?MODULE, merge, [list(elements(Vs))]} || length(Vs) > 0] ++
+          [{call, ?MODULE, descends, [elements(Vs), elements(Vs)]} || length(Vs) > 0] ++
+          [{call, ?MODULE, dominates, [elements(Vs), elements(Vs)]} || length(Vs) > 0]
           ).
 
 %% Next state transformation, S is the current state
 next_state(S,_V,{call,_,get_counter,[_, _]}) ->
     S;
 next_state(S,_V,{call,_,descends,[_, _]}) ->
+    S;
+next_state(S,_V,{call,_,dominates,[_, _]}) ->
     S;
 next_state(#state{vclocks=Vs}=S,V,{call,_,_,_}) ->
     S#state{vclocks=Vs ++ [V]}.
@@ -48,8 +51,10 @@ postcondition(_S, {call, _, get_counter, _}, {MRes, Res}) ->
     MRes == Res;
 postcondition(_S, {call, _, descends, _}, {MRes, Res}) ->
     MRes == Res;
+postcondition(_S, {call, _, dominates, _}, {MRes, Res}) ->
+    MRes == Res;
 postcondition(_S, {call, _, merge, [Items]}, {MRes, Res}) ->
-    model_compare(MRes, Res) andalso 
+    model_compare(MRes, Res) andalso
         lists:all(fun({_, V}) ->
                           vclock:descends(Res, V)
                   end, Items);
@@ -64,7 +69,8 @@ prop_vclock() ->
             begin
                 {H,S,Res} = run_commands(?MODULE,Cmds),
                 aggregate(command_names(Cmds),
-                          pretty_commands(?MODULE,Cmds, {H,S,Res}, Res == ok))
+                          collect({num_vclocks, length(S#state.vclocks) div 10},
+                                  pretty_commands(?MODULE,Cmds, {H,S,Res}, Res == ok)))
             end).
 
 gen_actor_id() ->
@@ -74,7 +80,7 @@ gen_vclock() ->
     ?SIZED(Size, gen_vclock(Size)).
 
 gen_vclock(Size) ->
-    ?LAZY(frequency([{1, {new_model(), vclock:fresh()}} ] ++ 
+    ?LAZY(frequency([{1, {new_model(), vclock:fresh()}} ] ++
               [ {3, gen_incr_vclock(Size - 1)} || Size > 0] ++
               [ {1, gen_merged_vclock(Size div 2)} || Size > 0 ])).
 
@@ -91,11 +97,12 @@ gen_incr_vclock(Size) ->
 fresh() ->
     {new_model(), vclock:fresh()}.
 
+dominates({AM, AV}, {BM, BV}) ->
+    {model_descends(AM, BM) andalso AM =/= BM,
+     vclock:dominates(AV, BV)}.
+
 descends({AM, AV}, {BM, BV}) ->
-    {lists:all(fun({Actor, Count}) ->
-                       Count >= orddict:fetch(Actor, BM)
-               end, 
-               AM),
+    {model_descends(AM, BM),
      vclock:descends(AV, BV)}.
 
 get_counter(A, {M, V}) ->
@@ -116,9 +123,15 @@ new_model() ->
     orddict:from_list([{ID, 0} || ID <- ?ACTOR_IDS]).
 
 model_compare(M, V) ->
-    lists:all(fun(A) -> 
-                      orddict:fetch(A,M) == vclock:get_counter(A,V) 
-              end, 
+    lists:all(fun(A) ->
+                      orddict:fetch(A,M) == vclock:get_counter(A,V)
+              end,
               ?ACTOR_IDS).
+
+model_descends(AM, BM) ->
+   lists:all(fun({Actor, Count}) ->
+                     Count >= orddict:fetch(Actor, BM)
+             end,
+             AM).
 
 -endif.
