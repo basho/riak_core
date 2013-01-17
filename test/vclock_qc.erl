46feefb081f63befa21c85e132e3d5e22a6ebb98
Sean Cribbs <sean@basho.com>
2013-01-16 21:31:31 -0700

    Generate the model along with the concrete in gen_vclock().

--- test/vclock_qc.erl
+++ test/vclock_qc.erl
@@ -20,7 +20,7 @@ eqc_test_() ->
 %% Initialize the state
 initial_state() ->
     #state{vclock=vclock:fresh(),
-           model=orddict:from_list([{ID, 0} || ID <- ?ACTOR_IDS])
+           model=new_model()
           }.
 
 %% Command generator, S is the state
@@ -28,7 +28,7 @@ command(S) ->
     oneof([
            {call, vclock, increment, [gen_actor_id(), S#state.vclock]},
            {call, vclock, get_counter, [gen_actor_id(), S#state.vclock]},
-           {call, vclock, merge, [[gen_vclock(), S#state.vclock]]}
+           {call, ?MODULE, merge, [gen_vclock(), S#state.vclock]}
           ]).
 
 %% Next state transformation, S is the current state
@@ -37,8 +37,8 @@ next_state(S,V,{call,vclock,increment,[ActorId, _]}) ->
             model=orddict:update_counter(ActorId, 1, S#state.model)};
 next_state(S,_V,{call,vclock,get_counter,[_, _]}) ->
     S;
-next_state(S,V,{call,vclock,merge,[[Other,_VClock]]}) ->
-    S#state{vclock=V, model=model_merge(S#state.model, Other)};
+next_state(S,V,{call,_,merge,[{O,_}, _VClock]}) ->
+    S#state{vclock=V, model=model_merge(S#state.model, O)};
 next_state(S,_V,{call,_,_,_}) ->
     S.
 
@@ -56,11 +56,12 @@ postcondition(#state{model=M},{call,vclock,increment,[ActorId, Vclock]},Res) ->
 postcondition(#state{model=M}, {call,vclock,get_counter,[ActorId, _]}, Res) ->
     %% The counter for a given actor is the same as the model
     Res == orddict:fetch(ActorId, M);
-postcondition(#state{model=M}, {call,vclock,merge,[[Other,VClock]]}, Res) ->
-    %% The counter for a given actor is the same as the model
-    Merged = model_merge(M,Other),
+postcondition(#state{model=M}, {call,_,merge,[{OM,Other},VClock]}, Res) ->
+    Merged = model_merge(M, OM),
+    %% The merged state has all the same actor counts
     lists:all(fun(A) -> orddict:fetch(A,Merged) == vclock:get_counter(A,Res) end, 
               ?ACTOR_IDS) andalso
+        %% The merged vector clock descends from both inputs
         vclock:descends(Res, Other) andalso vclock:descends(Res, VClock);
 postcondition(_S, _C, _Res) ->
     true.
@@ -79,14 +80,21 @@ gen_actor_id() ->
 
 gen_vclock() ->
     frequency([
-               {1, vclock:fresh()},
-               {3, ?LAZY(?LET({Actor, Vc},
-                              {gen_actor_id(), gen_vclock()},
-                              vclock:increment(Actor, Vc)))}
+               {1, {new_model(), vclock:fresh()}},
+               {3, ?LAZY(gen_incr_vclock())}
               ]).
 
-model_merge(M, V) ->
-    orddict:map(fun(A,C) ->
-                        erlang:max(C,vclock:get_counter(A, V)) 
-                end, M).
+gen_incr_vclock() ->
+    ?LET({Actor, {M,Vc}},
+         {gen_actor_id(), gen_vclock()},
+         {orddict:update_counter(Actor, 1, M), vclock:increment(Actor, Vc)}).
+
+merge({_,O}, V) ->
+    vclock:merge([O,V]).
+
+model_merge(M,OM) ->
+    orddict:merge(fun(_K,A,B) -> erlang:max(A,B) end, M, OM).
+
+new_model() ->
+    orddict:from_list([{ID, 0} || ID <- ?ACTOR_IDS]).
 -endif.
