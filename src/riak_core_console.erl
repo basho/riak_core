7ae213fe706280d5c022c6bffaf0deb6e8fd4a76
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2013-03-11 23:30:08 -0400

    Add function to return proplist of nodes and claim.

--- src/riak_core_console.erl
+++ src/riak_core_console.erl
@@ -22,7 +22,13 @@
 -export([member_status/1, ring_status/1, print_member_status/2,
          stage_leave/1, stage_remove/1, stage_replace/1,
          stage_force_replace/1, print_staged/1, commit_staged/1,
-         clear_staged/1, transfer_limit/1, pending_claim_percentage/2]).
+         clear_staged/1, transfer_limit/1, pending_claim_percentage/2,
+         pending_nodes_and_claim_percentages/1]).
+
+%% @doc Return list of nodes, current and future claim.
+pending_nodes_and_claim_percentages(Ring) ->
+    Nodes = lists:keysort(2, riak_core_ring:all_member_status(Ring)),
+    [{Node, pending_claim_percentage(Ring, Node)} || Node <- Nodes].
 
 %% @doc Return for a given ring and node, percentage currently owned and
 %% anticipated after the transitions have been completed.
