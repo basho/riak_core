fc5db8c5cc40913d358306cdf436091e53c826f0
Scott Lystig Fritchie <slfritchie@snookles.com>
2013-05-11 09:45:52 +0900

    Add PULSE instrumentation to riak_core_send_msg.erl

--- src/riak_core_send_msg.erl
+++ src/riak_core_send_msg.erl
@@ -27,6 +27,15 @@
          send_event_unreliable/2,
          bang_unreliable/2]).
 
+-ifdef(TEST).
+-ifdef(PULSE).
+-compile(export_all).
+-compile({parse_transform, pulse_instrument}).
+-compile({pulse_replace_module, [{gen_fsm, pulse_gen_fsm},
+                                 {gen_server, pulse_gen_server}]}).
+-endif.
+-endif.
+
 %% NOTE: We'ed peeked inside gen_server.erl's guts to see its internals.
 reply_unreliable({To, Tag}, Reply) ->
     bang_unreliable(To, {Tag, Reply}).
