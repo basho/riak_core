4ad05bfeb5e24547fa53dc66fc47107152e9f4c8
Chris Tilt <chris.tilt@gmail.com>
2013-08-29 16:38:51 -0700

    Copyright for include file

--- include/riak_core_token_manager.hrl
+++ include/riak_core_token_manager.hrl
@@ -1,3 +1,22 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2013 Basho Technologies, Inc.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
 -type tm_token() :: any().
 -type tm_meta()  :: {atom(), any()}.                %% meta data to associate with a token
 -type tm_period() :: pos_integer().                 %% refill period in milliseconds
