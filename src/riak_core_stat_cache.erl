3ea67a812c49866473f782494dac0ce5ab105b05
Engel A. Sanchez <engel@basho.com>
2013-09-04 14:51:53 -0400

    Fix meck related unit test problems
    Meck now needs a couple of flags in meck:new for some of our tests:
    * non_strict so we can create a fresh non-existent module
    * no_link so it doesn't link to the processes created with a meck
    * module. Bad when we kill things on purpose.
    
    Trying non_strict meck flag in riak_core_stat_cache.
    The build failed with an undefined_module error setting up this text. I
    can not get that to happen locally, but this flag has fixed similar
    problems in other tests.

--- src/riak_core_stat_cache.erl
+++ src/riak_core_stat_cache.erl
@@ -272,7 +272,7 @@ cache_test_() ->
     {setup,
      fun() ->
              folsom:start(),
-             [meck:new(Mock, [passthrough]) || Mock <- ?MOCKS],
+             [meck:new(Mock, [non_strict, passthrough]) || Mock <- ?MOCKS],
              riak_core_stat_cache:start_link()
      end,
      fun(_) ->
