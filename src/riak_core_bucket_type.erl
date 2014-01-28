4289d752ba5bb21561a396ce3e34a7594eec8240
Kelly McLaughlin <kelly@basho.com>
2014-01-28 12:30:31 -0700

    Add property_hash/1 function to riak_core_bucket_type module
    Add riak_core_bucket_type:property_hash/1 function to provide a
    convenient way to calculate a hash of certain bucket type properties
    whose values could impact the treatment of buckets created with a
    particular bucket type.

--- src/riak_core_bucket_type.erl
+++ src/riak_core_bucket_type.erl
@@ -102,7 +102,8 @@
          itr_next/1,
          itr_done/1,
          itr_value/1,
-         itr_close/1]).
+         itr_close/1,
+         property_hash/1]).
 
 -export_type([bucket_type/0]).
 -type bucket_type()       :: binary().
@@ -112,7 +113,7 @@
                               true -> X;
                               false -> E
                           end).
-
+-define(HASHED_BUCKET_TYPES_PROPS, [consistent, datatype, n_val, allow_mult, last_write_wins]).
 
 %% @doc The hardcoded defaults for all bucket types.
 -spec defaults() -> bucket_type_props().
@@ -218,3 +219,13 @@ itr_value(It) ->
 -spec itr_close(riak_core_metadata:iterator()) -> ok.
 itr_close(It) ->
     riak_core_metadata:itr_close(It).
+
+%% @doc Returns a hash of the bucket type properties whose values may
+%% have implications on the treatment or handling of buckets created
+%% using the bucket type.
+-spec property_hash(undefined | bucket_type_props()) -> undefined | integer().
+property_hash(undefined) ->
+    undefined;
+property_hash(Props) when is_list(Props) ->
+    erlang:phash2([lists:keyfind(Prop, 1, Props) ||
+                      Prop <- ?HASHED_BUCKET_TYPES_PROPS]).
