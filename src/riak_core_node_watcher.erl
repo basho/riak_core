619bd939b8474a6c9192dab962a0b9d766534866
Sean Cribbs <sean@basho.com>
2014-02-18 09:16:04 -0600

    Thread through state changes and match return values of cancel_timer

--- src/riak_core_node_watcher.erl
+++ src/riak_core_node_watcher.erl
@@ -325,8 +325,7 @@ handle_info({'DOWN', Mref, _, _Pid, _Info}, State) ->
 
             %% Update our list of active services and ETS table
             Services = ordsets:del_element(Id, State#state.services),
-            S3 = S2#state { services = Services },
-            local_update(S3),
+            S3 = local_update(S2#state { services = Services }),
             {noreply, update_avsn(S3)}
     end;
 
@@ -401,7 +400,7 @@ schedule_broadcast(State) ->
         undefined ->
             ok;
         OldTref ->
-            erlang:cancel_timer(OldTref),
+            _ = erlang:cancel_timer(OldTref),
             ok
     end,
     Interval = app_helper:get_env(riak_core, gossip_interval),
@@ -501,7 +500,7 @@ local_delete(State) ->
     case node_delete(node()) of
         [] ->
             %% No services changed; no local notification required
-            State;
+            ok;
 
         AffectedServices ->
             riak_core_node_watcher_events:service_update(AffectedServices)
@@ -627,7 +626,7 @@ remove_health_check(ServiceId, State) ->
         error ->
             Healths;
         {ok, Check} ->
-            health_fsm(remove, ServiceId, Check),
+            {_, _} = health_fsm(remove, ServiceId, Check),
             orddict:erase(ServiceId, Healths)
     end,
     State#state{health_checks = Healths2}.
@@ -711,8 +710,9 @@ health_fsm(checking, {'EXIT', Pid, Cause}, Service, #health_check{checking_pid =
 health_fsm(waiting, suspend, _Service, InCheck) ->
     case InCheck#health_check.interval_tref of
         undefined -> ok;
-        _ -> erlang:cancel_timer(InCheck#health_check.interval_tref),
-             ok
+        _ ->
+            _ = erlang:cancel_timer(InCheck#health_check.interval_tref),
+            ok
     end,
     {ok, suspend, InCheck#health_check{interval_tref = undefined}};
 
@@ -723,8 +723,9 @@ health_fsm(waiting, check_health, Service, InCheck) ->
 health_fsm(waiting, remove, _Service, InCheck) ->
     case InCheck#health_check.interval_tref of
         undefined -> ok;
-        Tref -> erlang:cancel_timer(Tref),
-                ok
+        Tref ->
+            _ = erlang:cancel_timer(Tref),
+            ok
     end,
     OutCheck = InCheck#health_check{interval_tref = undefined},
     {remove, waiting, OutCheck};
@@ -754,8 +755,9 @@ start_health_check(Service, #health_check{checking_pid = undefined} = CheckRec) 
     Pid = CheckRec#health_check.service_pid,
     case CheckRec#health_check.interval_tref of
         undefined -> ok;
-        Tref -> erlang:cancel_timer(Tref),
-                ok
+        Tref ->
+            _ = erlang:cancel_timer(Tref),
+            ok
     end,
     CheckingPid = proc_lib:spawn_link(fun() ->
         case erlang:apply(Mod, Func, [Pid | Args]) of
