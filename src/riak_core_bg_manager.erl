3c51eab94083e5f9e0b2f8e23d8d5e0acf012e4c
Micah Warren <mwarren@basho.com>
2014-02-19 15:06:03 -0600

    Removed unneeded macro.
    The table is not given to the bg_manager. The supervising process
    creates the tables named and public. The bg_manager always refers to
    the tables by name, so there's waiting for a transfer.

--- src/riak_core_bg_manager.erl
+++ src/riak_core_bg_manager.erl
@@ -129,8 +129,6 @@
 
 -define(SERVER, ?MODULE).
 
--define(NOT_TRANSFERED(S), S#state.info_table == undefined orelse S#state.entry_table == undefined).
-
 %%%===================================================================
 %%% API
 %%%===================================================================
@@ -564,15 +562,11 @@ validate_hold(_Obj, _TableId) -> %% tokens don't monitor processes
     ok.
 
 %% @doc Update state with bypassed status and store to ETS
-update_bypassed(_Bypassed, State) when ?NOT_TRANSFERED(State) ->
-    State;
 update_bypassed(Bypassed, State=#state{info_table=TableId}) ->
     ets:insert(TableId, {bypassed, Bypassed}),
     State#state{bypassed=Bypassed}.
 
 %% @doc Update state with enabled status and store to ETS
-update_enabled(_Enabled, State) when ?NOT_TRANSFERED(State) ->
-    State;
 update_enabled(Enabled, State=#state{info_table=TableId}) ->
     ets:insert(TableId, {enabled, Enabled}),
     State#state{enabled=Enabled}.
@@ -608,8 +602,6 @@ do_handle_call_exception(Function, Args, State) ->
     end.
 
 %% @doc Throws {unregistered, Resource} for unknown Lock.
-do_disable_lock(_Lock, _Kill, State) when ?NOT_TRANSFERED(State) ->
-    {noreply, State};
 do_disable_lock(Lock, Kill, State) ->
     Info = resource_info(Lock, State),
     enforce_type_or_throw(Lock, lock, Info),
@@ -637,9 +629,6 @@ do_set_token_rate(Token, Rate, State) ->
             {reply, Error, State}
     end.
 
-do_get_type_info(_Type, State) when ?NOT_TRANSFERED(State) ->
-    %% Table not trasnferred yet.
-    [];
 do_get_type_info(Type, State) ->
     S = fun({R,_T,E,L}) -> {R,E,L} end,
     Resources = all_registered_resources(Type, State),
@@ -647,8 +636,6 @@ do_get_type_info(Type, State) ->
     {reply, Infos, State}.
 
 %% Returns empty if the ETS table has not been transferred to us yet.
-do_resource_limit(_Type, _Resource, State) when ?NOT_TRANSFERED(State) ->
-    {reply, undefined, state};
 do_resource_limit(_Type, Resource, State) ->
     Info = resource_info(Resource, State),
     Rate = ?resource_limit(Info),
@@ -700,8 +687,6 @@ limit(#resource_info{type=token, limit={_Period,MaxCount}}) -> MaxCount.
 %% @private
 %% @doc Release the resource associated with the given reference. This is mostly
 %%      meaningful for locks.
-release_resource(_Ref, State) when ?NOT_TRANSFERED(State) ->
-    State;
 release_resource(Ref, State=#state{entry_table=TableId}) ->
     %% There should only be one instance of the object, but we'll zap all that match.
     Given = [Obj || Obj <- ets:match_object(TableId, {{given, '_'},'_'})],
@@ -758,8 +743,6 @@ update_resource_info(Resource, Fun, Default, State=#state{info_table=TableId}) -
     State.
 
 %% @doc Throws unregistered for unknown Resource
-resource_info(_Resource, State) when ?NOT_TRANSFERED(State) ->
-    throw(table_id_undefined);
 resource_info(Resource, #state{info_table=TableId}) ->
     Key = {info,Resource},
     case ets:lookup(TableId, Key) of
@@ -783,8 +766,6 @@ reschedule_token_refills(State) ->
     [schedule_refill_tokens(Token, State) || Token <- Tokens].
  
 %% Schedule a timer event to refill tokens of given type
-schedule_refill_tokens(_Token, State) when ?NOT_TRANSFERED(State) ->
-    ok;
 schedule_refill_tokens(Token, State) ->
     case ?resource_limit(resource_info(Token, State)) of
         undefined ->
@@ -842,10 +823,6 @@ try_get_resource(true, Resource, Type, Pid, Meta, State) ->
                              {reply, max_concurrency, #state{}}
                                  | {reply, {ok, #state{}}}
                                  | {reply, {{ok, reference()}, #state{}}}.
-do_get_resource(_Resource, _Type, _Pid, _Meta, State) when ?NOT_TRANSFERED(State) ->
-    %% Table transfer has not occurred yet. Reply "max_concurrency" so that callers
-    %% will try back later, hopefully when we have our table back.
-    {reply, max_concurrency, State};
 %% @doc When the API is bypassed, we ignore concurrency limits.
 do_get_resource(Resource, Type, Pid, Meta, State=#state{bypassed=true}) ->
     {Result, State2} = try_get_resource(true, Resource, Type, Pid, Meta, State),
