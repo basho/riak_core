2f30c36ed9ef0a0e2a6f2f5d60f37de5f2939f3a
Sean Cribbs <sean@basho.com>
2014-02-21 17:45:58 -0600

    Capabilities can be two-tuples as well as atoms.

--- src/riak_core_capability.erl
+++ src/riak_core_capability.erl
@@ -90,7 +90,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
--type capability() :: atom().
+-type capability() :: atom() | {atom(), atom()}.
 -type mode() :: term().
 
 -record(capability, {supported :: [mode()],
