1c66bece57c83342796a5bf94eb67bfaa114cacc
Andrew Thompson <andrew@hijacked.us>
2014-03-04 13:45:48 -0500

    Update tools.mk to 0.5.3 and add dialyzer ignore file

--- tools.mk
+++ tools.mk
@@ -1,47 +1,68 @@
+REBAR ?= ./rebar
+
+compile-no-deps:
+	${REBAR} compile skip_deps=true
+
 test: compile
-	./rebar eunit skip_deps=true
+	${REBAR} eunit skip_deps=true
 
 docs:
-	./rebar doc skip_deps=true
+	${REBAR} doc skip_deps=true
 
 xref: compile
-	./rebar xref skip_deps=true
+	${REBAR} xref skip_deps=true
 
-PLT ?= $(HOME)/.riak_combo_dialyzer_plt
+PLT ?= $(HOME)/.combo_dialyzer_plt
 LOCAL_PLT = .local_dialyzer_plt
 DIALYZER_FLAGS ?= -Wunmatched_returns
 
 ${PLT}: compile
-ifneq (,$(wildcard $(PLT)))
-	dialyzer --check_plt --plt $(PLT) --apps $(DIALYZER_APPS) && \
-		dialyzer --add_to_plt --plt $(PLT) --output_plt $(PLT) --apps $(DIALYZER_APPS) ; test $$? -ne 1
-else
-	dialyzer --build_plt --output_plt $(PLT) --apps $(DIALYZER_APPS); test $$? -ne 1
-endif
-
-${LOCAL_PLT}: compile
-ifneq (,$(wildcard deps/*))
-ifneq (,$(wildcard $(LOCAL_PLT)))
-	dialyzer --check_plt --plt $(LOCAL_PLT) deps/*/ebin  && \
-		dialyzer --add_to_plt --plt $(LOCAL_PLT) --output_plt $(LOCAL_PLT) deps/*/ebin ; test $$? -ne 1
-else
-	dialyzer --build_plt --output_plt $(LOCAL_PLT) deps/*/ebin ; test $$? -ne 1
-endif
-endif
-
-dialyzer: ${PLT} ${LOCAL_PLT}
-	@echo "==> $(shell basename $(shell pwd)) (dialyzer)"
-	@if [ -f $(LOCAL_PLT) ]; then \
-		dialyzer $(DIALYZER_FLAGS) --plts $(PLT) $(LOCAL_PLT) -c ebin; \
+	@if [ -f $(PLT) ]; then \
+		dialyzer --check_plt --plt $(PLT) --apps $(DIALYZER_APPS) && \
+		dialyzer --add_to_plt --plt $(PLT) --output_plt $(PLT) --apps $(DIALYZER_APPS) ; test $$? -ne 1; \
 	else \
-		dialyzer $(DIALYZER_FLAGS) --plts $(PLT) -c ebin; \
+		dialyzer --build_plt --output_plt $(PLT) --apps $(DIALYZER_APPS); test $$? -ne 1; \
 	fi
 
+${LOCAL_PLT}: compile
+	@if [ -d deps ]; then \
+		if [ -f $(LOCAL_PLT) ]; then \
+			dialyzer --check_plt --plt $(LOCAL_PLT) deps/*/ebin  && \
+			dialyzer --add_to_plt --plt $(LOCAL_PLT) --output_plt $(LOCAL_PLT) deps/*/ebin ; test $$? -ne 1; \
+		else \
+			dialyzer --build_plt --output_plt $(LOCAL_PLT) deps/*/ebin ; test $$? -ne 1; \
+		fi \
+	fi
+
+dialyzer-run:
+	@echo "==> $(shell basename $(shell pwd)) (dialyzer)"
+	@if [ -f $(LOCAL_PLT) ]; then \
+		PLTS="$(PLT) $(LOCAL_PLT)"; \
+	else \
+		PLTS=$(PLT); \
+	fi; \
+	if [ -f dialyzer.ignore-warnings ]; then \
+		if [ $$(grep -cvE '[^[:space:]]' dialyzer.ignore-warnings) -ne 0 ]; then \
+			echo "ERROR: dialyzer.ignore-warnings contains a blank/empty line, this will match all messages!"; \
+			exit 1; \
+		fi; \
+		dialyzer $(DIALYZER_FLAGS) --plts $${PLTS} -c ebin > dialyzer_warnings ; \
+		egrep -v "^\s*(done|Checking|Proceeding|Compiling)" dialyzer_warnings | grep -F -f dialyzer.ignore-warnings -v > dialyzer_unhandled_warnings ; \
+		cat dialyzer_unhandled_warnings ; \
+		[ $$(cat dialyzer_unhandled_warnings | wc -l) -eq 0 ] ; \
+	else \
+		dialyzer $(DIALYZER_FLAGS) --plts $${PLTS} -c ebin; \
+	fi
+
+dialyzer-quick: compile-no-deps dialyzer-run
+
+dialyzer: ${PLT} ${LOCAL_PLT} dialyzer-run
+
 cleanplt:
-	@echo 
+	@echo
 	@echo "Are you sure?  It takes several minutes to re-build."
 	@echo Deleting $(PLT) and $(LOCAL_PLT) in 5 seconds.
-	@echo 
+	@echo
 	sleep 5
 	rm $(PLT)
 	rm $(LOCAL_PLT)
