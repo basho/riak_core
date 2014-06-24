47538059c60d55c5d81c6a8d5fd28a1a93da2e17
Ulf Wiger <ulf@feuerlabs.com>
2014-06-24 10:55:54 +0200

    testing after rebase

--- src/riak_core_stat.erl
+++ src/riak_core_stat.erl
@@ -46,9 +46,32 @@ start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
 register_stats() ->
-    [(catch exometer_entry:delete([?APP,Name])) || {Name,_} <- stats()],
-    [register_stat({?APP, Name}, Type) || {Name, Type} <- stats()],
-    riak_core_stat_cache:register_app(?APP, {?MODULE, produce_stats, []}).
+    register_stats(common, system_stats()),
+    register_stats(?APP, stats()).
+
+%% @spec register_stats(App, Stats) -> ok
+%% @doc (Re-)Register a list of metrics for App.
+register_stats(App, Stats) ->
+    P = prefix(),
+    lists:foreach(fun(Stat) ->
+			  register_stat(P, App, Stat)
+		  end, Stats).
+
+register_stat(P, App, Stat) ->
+    {Name, Type, Opts} = case Stat of
+			     {N, T}     -> {N, T, []};
+			     {N, T, Os} -> {N, T, Os}
+			 end,
+    exometer:re_register(stat_name(P,App,Name), Type, Opts).
+
+stat_name(P, App, N) when is_atom(N) ->
+    stat_name_([P, App, N]);
+stat_name(P, App, N) when is_list(N) ->
+    stat_name_([P, App | N]).
+
+stat_name_([P, [] | Rest]) -> [P | Rest];
+stat_name_(N) -> N.
+
 
 %% @spec get_stats() -> proplist()
 %% @doc Get the current aggregation of stats.
@@ -114,10 +137,9 @@ stats() ->
      {converge_delay, duration},
      {rebalance_delay, duration}].
 
-register_stat(Name, Type) when is_tuple(Name) ->
-    exometer_entry:new(tuple_to_list(Name), Type);
-register_stat(Name, Type) when is_list(Name) ->
-    exometer_entry:new(Name, Type).
+system_stats() ->
+    [{cpu_stats, cpu, [{sample_interval, 5000}]}].
+
 
 gossip_stats() ->
     lists:flatten([backwards_compat(Stat, Type, riak_core_stat_q:calc_stat({{?APP, Stat}, Type})) ||
