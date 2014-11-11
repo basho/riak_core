73bb06f85b5e2b5d089e21c0c337b293f87efe49
John R. Daily <git@epep.us>
2014-11-11 15:56:18 -0500

    Standard license header

--- test/handoff_manager_eqc.erl
+++ test/handoff_manager_eqc.erl
@@ -1,9 +1,21 @@
-%%% @author John Daily <jd@epep.us>
-%%% @copyright (C) 2014, John Daily
-%%% @doc
-%%%
-%%% @end
-%%% Created : 10 Oct 2014 by John Daily <jd@epep.us>
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2014 Basho Technologies, Inc.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
 
 %%% TODO
 %% Transfer lifecycle testing (part of PR#630)
