55edde2622e2ca3957d9ebccc9ea4089f4c88e14
John R. Daily <git@epep.us>
2015-07-13 17:10:31 -0400

    Drop the rather hackish create_plan overloading, and fix(?) ring math for preflist generation

--- src/riak_core_coverage_fsm.erl
+++ src/riak_core_coverage_fsm.erl
@@ -203,6 +203,15 @@ maybe_start_timeout_timer(Timeout) ->
     gen_fsm:start_timer(Timeout, {timer_expired, Timeout}),
     ok.
 
+%% @private
+find_plan(#vnode_coverage{}=Plan, _NVal, _PVC, _ReqId, _Service) ->
+    riak_core_coverage_plan:interpret_plan(Plan);
+find_plan(VNodeTarget, NVal, PVC, ReqId, Service) ->
+    riak_core_coverage_plan:create_plan(VNodeTarget,
+                                        NVal,
+                                        PVC,
+                                        ReqId,
+                                        Service).
 
 %% @private
 initialize(timeout, StateData0=#state{mod=Mod,
@@ -216,11 +225,11 @@ initialize(timeout, StateData0=#state{mod=Mod,
                                       timeout=Timeout,
                                       vnode_master=VNodeMaster,
                                       plan_fun = PlanFun}) ->
-    CoveragePlan = riak_core_coverage_plan:create_plan(VNodeSelector,
-                                                       NVal,
-                                                       PVC,
-                                                       ReqId,
-                                                       NodeCheckService),
+    CoveragePlan = find_plan(VNodeSelector,
+                             NVal,
+                             PVC,
+                             ReqId,
+                             NodeCheckService),
     case CoveragePlan of
         {error, Reason} ->
             Mod:finish({error, Reason}, ModState);
