d994ae13bd6136c963257a74d8423bac4950ba7a
John R. Daily <git@epep.us>
2015-12-17 10:09:57 -0500

    copyright date update

--- src/riak_core_coverage_plan.erl
+++ src/riak_core_coverage_plan.erl
@@ -2,7 +2,7 @@
 %%
 %% riak_core_coverage_plan: Create a plan to cover a minimal set of VNodes.
 %%
-%% Copyright (c) 2007-2011 Basho Technologies, Inc.  All Rights Reserved.
+%% Copyright (c) 2007-2015 Basho Technologies, Inc.  All Rights Reserved.
 %%
 %% This file is provided to you under the Apache License,
 %% Version 2.0 (the "License"); you may not use this file
