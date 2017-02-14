bb87dbc4101d27a3d1b440aa68ea6a38cc36d098
Doug Rohrer <drohrer@basho.com>
2017-02-14 11:49:09 -0500

    Make `riak_core_info_service_process` a `gen_server`. - Invoke the `shutdown` callback in `Mod:terminate`

--- src/riak_core_info_service_process.erl
+++ src/riak_core_info_service_process.erl
@@ -1,113 +1,69 @@
 -module(riak_core_info_service_process).
+-behaviour(gen_server).
 
 -record(state, {
     source = undefined :: riak_core_info_service:callback(),
-    handler = undefined :: riak_core_info_service:callback(),
-    shutdown = undefined :: riak_core_info_service:callback(),
-    parent :: pid(),
-    debug :: list()
+    response_handler = undefined :: riak_core_info_service:callback(),
+    shutdown = undefined :: riak_core_info_service:callback()
 }).
 
--export([start_link/4]).
+-export([start_link/4,
+         init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3]).
 
-%% Application messages are sent to this process directly from
-%% NIF-land instead of other Erlang modules, so this does not
-%% implement an OTP behavior.
-%%
-%% See http://erlang.org/doc/design_principles/spec_proc.html for
-%% information on these system message handlers.
--export([system_continue/3, system_terminate/4,
-         system_get_state/1, system_replace_state/2,
-         system_code_change/4, callback_router/1]).
 
-start_link(Registration, Shutdown, Source, Handler) ->
-    State = #state{shutdown = Shutdown,
-        source = Source,
-        handler = Handler,
-        parent = self(),
-        debug = sys:debug_options([])},
-    Pid = proc_lib:spawn_link(?MODULE, callback_router,
-                              [State]),
-    
-    %% Call the Registration function provided to let the registered system
-    %% know the Pid of its info process
+%% `gen_server' implementation
+
+start_link(Registration, Shutdown, Source, ResponseHandler) ->
+    {ok, Pid} = gen_server:start_link(?MODULE, [Shutdown, Source, ResponseHandler], []),
     apply_callback(Registration, [Pid]),
     {ok, Pid}.
+
+init([Shutdown, Source, ResponseHandler]) ->
+    State = #state{shutdown  = Shutdown,
+                   source  = Source,
+                   response_handler = ResponseHandler},
+    {ok, State}.
+
+handle_info({invoke, SourceParams, HandlerContext}, State) ->
+    handle_invoke_message(SourceParams, HandlerContext, State);
+
+handle_info(callback_shutdown, State) ->
+    handle_shutdown_message(State).
+
+terminate(_Reason, State) ->
+    handle_shutdown_message(State).
+
+code_change(_OldVsn, State, _Extra) ->
+    State.
+
+handle_call(_Request, _From, _State) ->
+    erlang:error(not_implemented).
+
+handle_cast(_Request, _State) ->
+    erlang:error(not_implemented).
+
+%% Private
+
+handle_invoke_message(SourceParams, HandlerContext,
+                      #state{source = Source,
+                             response_handler = ResponseHandler } = State) ->
+    Result = apply_callback(Source, SourceParams),
+    Reply = {Result, SourceParams, HandlerContext},
+    apply_callback(ResponseHandler, [Reply]),
+    {noreply, State}.
+
+handle_shutdown_message(#state{shutdown = Shutdown}=State) ->
+    apply_callback(Shutdown, [self()]),
+    {stop, normal, State}.
 
 apply_callback(undefined, _CallSpecificArgs) ->
     ok;
 apply_callback({Mod, Fun, StaticArgs}, CallSpecificArgs) ->
     %% Invoke the callback, passing static + call-specific args for this call
     erlang:apply(Mod, Fun, StaticArgs ++ CallSpecificArgs).
-
--spec callback_router(#state{}) -> ok.
-callback_router(#state{} = State) ->
-    receive
-        {invoke, SourceParams, HandlerContext}=Msg ->
-            handle_invoke_message(Msg, SourceParams, HandlerContext, State);
-        {system, From, Request} ->
-            handle_system_message(Request, From, State);
-        callback_shutdown=Msg ->
-            handle_shutdown_message(Msg, State);
-        Msg ->
-            handle_other_message(Msg, State)
-    end.
-
-handle_invoke_message(Msg, SourceParams, HandlerContext,
-                   #state{ debug = Debug0,
-                           source = Source,
-                           handler = Handler }=State) ->
-    Debug1 = sys:handle_debug(Debug0, fun write_debug/3,
-                              ?MODULE,
-                              {in, Msg}),
-    Result = apply_callback(Source, SourceParams),
-    Reply = {Result, SourceParams, HandlerContext},
-    apply_callback(Handler, [Reply]),
-    Debug = sys:handle_debug(Debug1, fun write_debug/3,
-                             ?MODULE,
-                             {out, Reply}),
-    callback_router(State#state{debug = Debug}).
-
-handle_shutdown_message(Msg, #state{debug = Debug, shutdown = Shutdown}) ->
-    _DebugOpts = sys:handle_debug(Debug, fun write_debug/3,
-                     ?MODULE,
-                     {in, Msg}),
-    apply_callback(Shutdown, [self()]),
-    ok.
-
--spec handle_system_message(Request::term(), From::{pid(), Tag:: _}, State :: #state{}) ->
-    no_return().
-handle_system_message(Request, From, #state{ debug=Debug, parent = Parent } = State) ->
-    %% No recursion here; `system_continue/3' will be invoked
-    %% if appropriate - note that this function will not "return"
-    sys:handle_system_msg(Request, From, Parent,
-                          ?MODULE, Debug, State).
-
-handle_other_message(Msg, #state{debug=Debug0} = State) ->
-    Debug = sys:handle_debug(Debug0, fun write_debug/3,
-                              ?MODULE,
-                              {in, Msg}),
-    callback_router(State#state{debug = Debug}).
-
-
-%% Made visible to `standard_io' via `sys:trace(Pid, true)'
-write_debug(Device, Event, Module) ->
-    io:format(Device, "~p event: ~p~n", [Module, Event]).
-
-%% System functions invoked by `sys:handle_system_msg'.
-system_continue(_Parent, Debug, State) ->
-    callback_router(State#state{debug = Debug}).
-
-system_terminate(Reason, _Parent, _Debug, _State) ->
-    exit(Reason).
-
-system_get_state(State) ->
-    {ok, State}.
-
-system_replace_state(StateFun, State) ->
-    NewState = StateFun(State),
-    {ok, NewState, NewState}.
-
-system_code_change(State, _Module, _OldVsn, _Extra) ->
-    {ok, State}.
 
