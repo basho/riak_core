1e31eff5f15a402a35bf5d1b52f10bfbd4b42d2d
Martin Sumner <martin.sumner@adaptip.co.uk>
2019-05-29 14:57:00 +0100

    Need to support old_seed behaviour
    Critial change - attempt to support consistent behaviour with random code wiht regards to return of old seed.
    
    This change is unproven at present.

--- src/riak_core_claim.erl
+++ src/riak_core_claim.erl
@@ -504,13 +504,14 @@ choose_claim_v3(Ring, _ClaimNode, Params) ->
 
     %% Seed the random number generator for predictable results
     %% run the claim, then put it back if possible
-    OldSeed = rand:seed(proplists:get_value(seed, Params, {1,2,3})),
+    OldSeed = rand:export_seed(),
+    _ = rand:seed(proplists:get_value(seed, Params, {1,2,3})),
     {NewOwners, NewMetrics} = claim_v3(Wants, Owners, Params),
     case OldSeed of
         undefined ->
             ok;
         _ ->
-            {_,_,_} = rand:seed(OldSeed),
+            _ = rand:seed(OldSeed),
             ok
     end,
 
