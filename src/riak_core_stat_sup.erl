a86ad613816ea34bddfe409bf9ef6ecdca59a9bb
banana <savannah.allsop@bet365.com>
2019-09-16 17:23:35 +0100

    removd riak_stat_profiles from the list as it is no longer a gen_server

--- src/riak_core_stat_sup.erl
+++ src/riak_core_stat_sup.erl
@@ -52,8 +52,7 @@ init([]) ->
     %% Note that exometer_core starts folsom now, so it is not needed here
     Children = lists:flatten(
                  [
-                   ?CHILD(riak_core_stats_sup, supervisor),
-                     ?CHILD(riak_stat_profiles, worker)
+                   ?CHILD(riak_core_stats_sup, supervisor)
                    ]),
 
     {ok, {{rest_for_one, 10, 10}, Children}}.
