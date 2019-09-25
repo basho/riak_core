c9bf4198d3a55fd4d98fc4cf40f2f051ffef5ac0
banana <savannah.allsop@bet365.com>
2019-09-25 16:54:39 +0100

    added find_persisted_data/1, to find the information on the stat polling that has been persisted in the metadata, created a private  fold function to use in terminate and in find the data, revamped the stop_server function and removed the get_stats function.

--- src/riak_stat_push.erl
+++ src/riak_stat_push.erl
@@ -12,54 +12,51 @@
 -export([
     setup/1,
     setdown/1,
-    setdown/0,
-    get_stats/0,
-    sanitise_data/1
+    sanitise_data/1,
+    find_persisted_data/1
 ]).
 
 -define(PUSHPREFIX, {riak_stat_push, term_to_binary(node())}).
+
 %%%-------------------------------------------------------------------
 %% @doc
 %% the default operation of this function is to start up the pushing
-%% and polling of stats from exometer to the UDP endpoint "setup".
+%% / polling of stats from exometer to the UDP/TCP endpoint.
 %% The ability to pass in an argument gives the added layer of
 %% functionality to choose the endpoint details quicker and easier,
-%% passing in arguments is optional. sanitise the argument passed in
-%% to retrieve the data from console, passing in information is
-%% optional as all the defaults are pulled out from the .config.
-%% similar to other stats functions the stats can be passed in to only
-%% poll those stats, however, only the enabled stats will return with
-%% values
 %%
 %% If the server is already setup and polling stats, setting up
-%% another will "restart" the gen_server.
+%% another will start up another gen_server.
 %% @end
 %%%-------------------------------------------------------------------
 -spec(setup(arg()) -> ok).
 setup(Arg) ->
     case sanitise_data(Arg) of
-        ok -> ok; %% io:format output
+        ok -> ok; %% ok == io:format output i.e. error msg
         {Protocol,SanitisedData} ->
-            Pid = start_server(Protocol,SanitisedData),
-            store_setup_info(Pid, Protocol,SanitisedData)
+            case check_args(Protocol,SanitisedData) of
+                ok -> ok;
+                {Protocol,SanitisedData} ->
+                    Pid = start_server(Protocol,SanitisedData),
+                    store_setup_info(Pid, Protocol,SanitisedData)
+            end
     end.
 
 store_setup_info(Pid, Protocol, {{Port, Instance, Sip},Stats}) ->
     Prefix = ?PUSHPREFIX,
     {Date0,Time0} = calendar:local_time(),
     Date = tuple_to_list(Date0), Time = tuple_to_list(Time0),
-    Key = {Date,Time,Protocol},
-    Value = {Port,Sip,Instance,Pid,{running,true},Stats},
-    riak_core_metadata:put(Prefix,Key,Value).
+    Key = {Date,Time,Protocol}, %% Key is tuple-object
+    Value =
+        #{port     => Port,
+          serverip => Sip,
+          instance => Instance,
+          pid      => Pid,
+          running  => true,
+          stats    => Stats}, %% Value is a map
+    riak_stat_meta:put(Prefix,Key,Value).
 
-start_server(undefined, {{_P, _I, _S}, _St}) ->
-    io:format("No Protocol type entered~n");
-start_server(_Pr, {{undefined, _I, _S},_St}) ->
-    io:format("No Port type entered~n");
-start_server(_Pr, {{_P, undefined, _S},_St}) ->
-    io:format("No Instance entered~n");
-start_server(_Pr, {{_P, _I, undefined},_St}) ->
-    io:format("Wrong Server Ip entered~n");
+
 start_server(udp, Data) ->
     riak_core_stats_sup:start_server(riak_stat_push_udp, Data);
 start_server(tcp, Arg) ->
@@ -68,6 +65,17 @@ start_server(http, _Arg) ->
     io:format("Error Unsupported : ~p~n",[http]);
 start_server(Protocol, _Arg) ->
     io:format("Error wrong type : ~p~n",[Protocol]).
+
+check_args('_', {{_P, _I, _S}, _St}) ->
+    io:format("No Protocol type entered~n");
+check_args(_Pr, {{'_', _I, _S},_St}) ->
+    io:format("No Port type entered~n");
+check_args(_Pr, {{_P, '_', _S},_St}) ->
+    io:format("No Instance entered~n");
+check_args(_Pr, {{_P, _I, '_'},_St}) ->
+    io:format("Wrong Server Ip entered~n");
+check_args(Protocol,{{Port,Instance,ServerIp},Stats}) ->
+    {Protocol,{{Port,Instance,ServerIp},Stats}}.
 
 %%%-------------------------------------------------------------------
 %% @doc
@@ -78,71 +86,89 @@ start_server(Protocol, _Arg) ->
 %% @end
 %%%-------------------------------------------------------------------
 -spec(setdown(arg()) -> ok).
-setdown() ->
-    setdown([]).
 setdown([]) ->
     io:format("No Argument entered ~n");
 setdown(Arg) ->
-    %%todo: put down a server depending on the information given. if instance is
-    %% given then it is all that is needed to terminate, if the port is given,
-    %% then the sip is needed
-    %% if the sip is given then the port is needed,
     {Protocol, SanitisedData} = sanitise_data(Arg),
     terminate_server(Protocol, SanitisedData).
 
-terminate_server(Protocol, {{Port,Instance,ServerIp},_Stats}) ->
-    ok;
+%% @doc change the undefined into
+-spec(terminate_server(arg(),arguments()) -> ok | error()).
 terminate_server(Protocol,SanitisedData) ->
-    riak_core_stats_sup:stop_server(riak_stat_push_udp).
+    foldinate(Protocol, SanitisedData).
 
-foldinate(_,{{undefined,_I,_S},_St})->
-    ok;
-foldinate(undefined,SanitisedData) ->
-    foldinate('_',SanitisedData);
-foldinate(Protocol,{{Port,Instance,ServerIp},_Stats}) ->
+foldinate('_',SanitisedData) ->
+    case check_args(fake_protocol,SanitisedData) of
+        %% dont care about the protocol not being entered
+        ok -> ok; %% something else isnt given
+        {_FakeProtocol, SanitisedData} ->
+            stop_server(fold('_',SanitisedData))
+    end;
+foldinate(Protocol,SanitisedData) ->
+    case check_args(Protocol,SanitisedData) of
+        ok -> ok;
+        {Protocol,SanitisedData} ->
+            stop_server(fold(Protocol,SanitisedData))
+    end.
+
+stop_server(ChildrenInfo) ->
+    lists:foreach(fun
+                      ({{_D,_T,_Pr},{_Po,_Sip,_I,Pid,_R,_S}}) ->
+                          riak_core_stats_sup:stop_server(Pid)
+                  end, ChildrenInfo).
+
+fold(Protocol,{{Port,Instance,ServerIp},_Stats}) ->
     riak_core_metadata:fold(
-        fun
-            ({MDate,MTime,MProtocol}, [{MPort,MSip,MInst,MPid,MRunTuple,MStats}],
-                {APort,AInstance,AServerIP})
-                when (APort == '_' orelse APort == MPort) andalso
-                 (AInstance == '_' orelse AInstance == MInst) andalso
-                 (AServerIP == '_' orelse AServerIP == MSip) ->
-                
+        fun              %% K-V in metadata todo: change to map
+            ({{MDate,MTime,MProtocol}, [{MPort,MSip,MInst,MPid,MRunTuple,MStats}]},
+                {Acc, APort,AInstance,AServerIP})
+                when    (    APort == MPort orelse     APort == '_')
+                andalso (AInstance == MInst orelse AInstance == '_')
+                andalso (AServerIP == MSip  orelse AServerIP == '_') ->
+                {[{{MDate,MTime,MProtocol},{MPort,MSip,MInst,MPid,MRunTuple,MStats}}|Acc],
+                    APort,AInstance,AServerIP};
 
-                                    ok
+            ({_K,_V},{Acc,APort,AInstance,AServerIP}) ->
+                {Acc,APort,AInstance,AServerIP}
         end,
-        {Port,Instance,ServerIp},?PUSHPREFIX, [{match, {'_','_',Protocol}}]
+        {[],Port,Instance,ServerIp},
+            ?PUSHPREFIX, [{match, {'_','_',Protocol}}]
     ).
 
+%%%-------------------------------------------------------------------
+
+find_persisted_data(Arg) ->
+    {Protocol, SanitisedData} = sanitise_data(Arg),
+     print_info(fold(Protocol,SanitisedData)).
+
+print_info([]) ->
+    io:fwrite("Nothing found~n");
+print_info(Info) ->
+    lists:foreach(fun
+                      ({{Date,Time,Protocol},{Port,Sip,Inst,Pid,Run,Stats}}) ->
+                          io:fwrite("~p/~p/~p ",Date),
+                          io:fwrite("~p:~p:~p ",Time),
+                          io:fwrite("~p       ",[Protocol]),
+                          io:fwrite("~p ~p ~p ~p ~p ~p~n",
+                              [Port,Sip,Inst,Pid,Run,Stats]);
+                      (_) -> ok
+                  end, Info).
+
+
+
 %%--------------------------------------------------------------------
-%% todo: make it so the sip instance and port should be added in,
-%% should not be undefined in the gen_server, otherwise fail.
--spec(sanitise_data(arg()) -> sanitised()).
 %% @doc
 %% Sanitise the data coming in, into the necessary arguments for setting
 %% up an endpoint, This is specific to the endpoint functions
 %% @end
+%%--------------------------------------------------------------------
+-spec(sanitise_data(arg()) -> sanitised()).
 sanitise_data([]) ->
     io:fwrite("No argument given~n");
 sanitise_data(Arg) ->
     [Opts | Stats] = break_up(Arg, "/"),
     List = break_up(Opts, ","),
     data_sanitise_2(List,Stats).
-%%
-%%    case Stats of
-%%        [] -> case riak_stat_console:data_sanitise(List) of
-%%                  {[riak|Rest],_,_,_} ->
-%%                      %% if only the stats needed is passed in:
-%%                      {data_sanitise_2_electric_boogaloo([]),[riak|Rest]};
-%%                  _ ->
-%%                      {data_sanitise_2_electric_boogaloo(List), ['_']}
-%%              end;
-%%        Data ->
-%%            {data_sanitise_2_electric_boogaloo(List),
-%%                riak_stat_console:data_sanitise(Data)}
-%%    end. %% sanitise the data and look for the protocol, if there is no
-        %% todo: if no protocol (udp/tcp) is not specified, return error.
-        %% todo: in the sanitisation perform the staritng of the server?
 
 break_up(Arg, Str) ->
     re:split(Arg, Str).
@@ -157,13 +183,15 @@ data_sanitise_2(List,Stats) ->
     case data_sanitise_2_electric_boogaloo(List) of
         ok -> ok;
         {Protocol, Port, Instance, ServerIp} ->
+            {Names,_,_,_} =
+                riak_stat_console:data_sanitise(Stats),
             {Protocol, {{Port, Instance, ServerIp},
-                riak_stat_console:data_sanitise(Stats)}}
+                Names}}
     end.
 
 data_sanitise_2_electric_boogaloo(Arg) ->
     data_sanitise_2_electric_boogaloo(
-        Arg, undefined,undefined,undefined,undefined).
+        Arg, '_','_','_','_').
 data_sanitise_2_electric_boogaloo([<<"protocol=", Pr/binary>>|Rest],
     Protocol, Port, Instance, ServerIp) ->
     NewProtocol =
@@ -196,22 +224,6 @@ data_sanitise_2_electric_boogaloo([<<"sip=", S/binary>> | Rest],
     data_sanitise_2_electric_boogaloo(Rest,Protocol,Port, Instance, NewIP);
 data_sanitise_2_electric_boogaloo([],Protocol, Port, Instance, ServerIp) ->
     {Protocol,Port, Instance, ServerIp}.
-
-%%%-------------------------------------------------------------------
-%% @doc
-%% retrieve all the stats out of riak_kv_status
-%% todo: improve the stat collection method
-%% @end
-%%%-------------------------------------------------------------------
--spec(get_stats() -> stats()).
-get_stats() ->
-    case application:get_env(riak_core, push_defaults_type, classic) of
-        classic ->
-            riak_kv_status:get_stats(web);
-        beta ->  %% get all stats and their values
-            riak_stat_exom:get_values(['_'])
-    end.
-
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
